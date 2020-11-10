a57cd6da2e4dc8be3a88ffcf385325aa97a65431
Hunter Olson <hunterolson@mail.weber.edu>
2020-11-10 15:13:15 -0700

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -7,3 +7,85 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 //TODO
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { pictures } from './pictures';
+
+let featuredPicture = document.querySelector('.featured');
+function main(x)
+{
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60')";
+    document.body.style.backgroundColor = "black";
+    if(x.matches)
+    {
+        featuredPicture.innerHTML = "";
+        for(let p of pictures)
+        {
+            let pictureThumb = document.getElementById('p' + p.id);
+            pictureThumb.classList.add('hidden');
+        }
+        for(let p of pictures)
+        {
+            if(p.id > 3)
+            {
+                break;
+            }
+            else
+            {
+                document.getElementById('p' + p.id).style.width = "100%";
+                document.querySelector('.pictures').style.width = "100%";
+                let pictureThumb = document.getElementById('p' + p.id);
+                pictureThumb.classList.remove('hidden');
+                pictureThumb.innerHTML = `
+                <img src ="${p.url}">
+                `;
+                pictureThumb.onclick = function () 
+                {
+                    selectPicture(p);
+                    document.querySelector('.featured').style.width = "100%";
+                }
+            }
+            
+            
+        }
+    }
+    else
+    {
+        featuredPicture.innerHTML = "";
+        for (let p of pictures) 
+        {
+            document.getElementById('p' + p.id).style.width = "100%";
+            document.querySelector('.pictures').style.width = "50%";
+            let pictureThumb = document.getElementById('p' + p.id);
+            pictureThumb.classList.remove('hidden');
+            pictureThumb.innerHTML = `
+            <img src ="${p.url}">
+            `;
+            pictureThumb.onclick = function () 
+            {
+                selectPicture(p);
+                document.querySelector('.featured').style.width = "25%";
+                document.querySelector('.item').style.marginLeft = "50%";
+            }
+            
+        }
+       
+    }
+
+    
+}
+function selectPicture(p)
+{
+    
+    featuredPicture.innerHTML = `
+    <div class = "item">
+        <img src="${p.url}" id="img" style="float: left;">
+        <div class = "caption">
+            <h2>${p.title}</h2><br>
+            <p>${p.description}</p>
+        </div>
+    </div>
+    `;
+}
+
+let x = window.matchMedia("(max-width: 575px)");
+main(x);
+x.addListener(main);
---
+++ src/javascripts/pictures.js
@@ -0,0 +1,74 @@
+export const pictures = [
+    {
+        "id": 1,
+        "title": "Snowflake",
+        "url": "https://images.unsplash.com/photo-1457269449834-928af64c684d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "A focused photo of a snowflake by Aaron Burden"
+    },
+    {
+        "id": 2,
+        "title": "Green Pine",
+        "url": "https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Snow field and green pine trees during daytime by Adam Chang"
+    },
+    {
+        "id": 3,
+        "title": "Wooden House",
+        "url": "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Wooden house near pine trees and pond coated with snow during daytime by Ian Keefe"
+    },
+    {
+        "id": 4,
+        "title": "Trees During Winter",
+        "url": "https://images.unsplash.com/photo-1516035645781-9f126e774e9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Trees covered with snow during daytime by Donnie Rosie"
+    },
+    {
+        "id": 5,
+        "title": "Wheat in the Wind",
+        "url": "https://images.unsplash.com/photo-1519749163903-90027c6224bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Brown wheats under gray sky by Joshua Fuller"
+    },
+    {
+        "id": 6,
+        "title": "Buried Cabin",
+        "url": "https://images.unsplash.com/photo-1453306458620-5bbef13a5bca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Snow-covered trees under blue cloudy sky by Bob Canning"
+    },
+    {
+        "id": 7,
+        "title": "Pine Cone",
+        "url": "https://images.unsplash.com/photo-1482331336918-c80fab6c90f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Shallow focus photography of pine cone by Aaron Burden"
+    },
+    {
+        "id": 8,
+        "title": "Round Red Fruits",
+        "url": "https://images.unsplash.com/photo-1484313544071-4d67c88b99be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Selective focus photo of frozen round red fruits by Galina N"
+    },
+    {
+        "id": 9,
+        "title": "Frozen Bubble",
+        "url": "https://images.unsplash.com/photo-1489674267075-cee793167910?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Shallow focus photography of bubble on leaves by Aaron Burden"
+    },
+    {
+        "id": 10,
+        "title": "Lakeside",
+        "url": "https://images.unsplash.com/photo-1459695452562-46cc57bef5f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Pine trees near water with fog by dominik Dombrowski"
+    },
+    {
+        "id": 11,
+        "title": "Mountains covered in Snow",
+        "url": "https://images.unsplash.com/photo-1455156218388-5e61b526818b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Snow-covered tree near body of water by Colby Thomas"
+    },
+    {
+        "id": 12,
+        "title": "Sunrise",
+        "url": "https://images.unsplash.com/photo-1485123263479-f5ea30a2f92d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+        "description": "Road in between trees covered by snow by Pigoff PhotographY"
+    }
+]
